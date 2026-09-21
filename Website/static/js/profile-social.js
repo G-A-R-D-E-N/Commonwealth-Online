213a07eda64e1280a4768ca43b7fafe952e40b49
Nomads_Reach <144523850+NomadsReach@users.noreply.github.com>
2026-09-21 13:29:39 -0400

    Wire recent server privacy setting

--- Website/static/js/profile-social.js
+++ Website/static/js/profile-social.js
@@ -31,7 +31,7 @@
   const loadDetails = async () => {
     const { data, error } = await client
       .from("user_profile_details")
-      .select("bio,faction,playstyle,is_public,show_presence,show_friends,show_joined_at,show_username_history")
+      .select("bio,faction,playstyle,is_public,show_presence,show_friends,show_joined_at,show_username_history,show_recent_servers")
       .eq("user_id", user.id)
       .single();
 
@@ -45,6 +45,7 @@
     form.elements.show_friends.checked = data.show_friends;
     form.elements.show_joined_at.checked = data.show_joined_at;
     form.elements.show_username_history.checked = data.show_username_history;
+    form.elements.show_recent_servers.checked = data.show_recent_servers;
   };
 
   const loadUsernameHistory = async () => {
@@ -381,6 +382,7 @@
       show_friends: form.elements.show_friends.checked,
       show_joined_at: form.elements.show_joined_at.checked,
       show_username_history: form.elements.show_username_history.checked,
+      show_recent_servers: form.elements.show_recent_servers.checked,
       updated_at: new Date().toISOString(),
     };
 
