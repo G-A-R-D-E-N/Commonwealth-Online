55f654e03962461c59359e8458319b11920e7f0a
Nomads_Reach <144523850+NomadsReach@users.noreply.github.com>
2026-09-21 13:45:47 -0400

    Load dedicated profile notification module

--- Website/src/routes/page-config.js
+++ Website/src/routes/page-config.js
@@ -405,6 +405,7 @@ const PUBLIC_PAGES = [
         "/static/js/script.js",
         "/static/js/profile.js",
         "/static/js/profile-social.js",
+        "/static/js/profile-notifications.js",
         "/static/js/profile-badges.js",
         "/static/js/profile-servers.js",
         "/static/js/profile-characters.js",
